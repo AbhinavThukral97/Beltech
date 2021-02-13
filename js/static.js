97f42e2d794fb557402d80b81ea35539bf127426
Beltech <64468226+beltechai@users.noreply.github.com>
2021-02-13 14:06:15 +0530

    Update static.js

--- js/static.js
+++ js/static.js
@@ -82,7 +82,7 @@ function generateFooter(page) {
   </div>
   </div>
   <p class="sm">
-    &copy; Copyright Beltech Green Pvt. Ltd. 2020 &middot; Made by <a>Abhinav Thukral</a>
+    &copy; Copyright Beltech Green Pvt. Ltd. 2021 &middot; </a>
   </p>
 </div>`;
   return footerHTML;
